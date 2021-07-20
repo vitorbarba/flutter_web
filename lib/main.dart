9f748e5546f0e7005df52a1afead39868589da2d
Vitor Alexandre de Barba <vitor.a.barba@gmail.com>
2021-07-20 11:52:31 -0300

    Title: todoList

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      title: 'todoList',
       initialBinding: AuthBinding(),
       home: Root(),
       theme: ThemeData.dark(),
